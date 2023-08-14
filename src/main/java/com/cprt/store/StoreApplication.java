b7a84e86256450fff3983cc6282825f5337eb462
diegoluanmartins <diegoluanmartins@gmail.com>
2023-08-14 20:55:33 -0300

    Chaged to use dp strategy for tax calculator

--- src/main/java/com/cprt/store/StoreApplication.java
+++ src/main/java/com/cprt/store/StoreApplication.java
@@ -4,12 +4,12 @@ import java.math.BigDecimal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cprt.store.budget.Budget;
 import com.cprt.store.tax.TaxCalculator;
-import com.cprt.store.tax.TaxType;
+import com.cprt.store.tax.TaxICMS;
+import com.cprt.store.tax.TaxISS;
 
 @SpringBootApplication
 public class StoreApplication {
@@ -17,16 +17,15 @@ public class StoreApplication {
 	private static final Logger logger = Logger.getLogger("main");
 	public static void main(String[] args) {
 		testBudget();
-		SpringApplication.run(StoreApplication.class, args);
-		
+		//SpringApplication.run(StoreApplication.class, args);
 	}
 
 	public static void testBudget(){
 		Budget budget = new Budget(new BigDecimal("100"));
 		TaxCalculator calculator = new TaxCalculator();
-		for(TaxType type : TaxType.values()){
-			logger.log(Level.INFO, "{0} = {1}", new Object[]{type.name(), calculator.calculate(budget, type)});
-		}
+		logger.log(Level.INFO, "{0} = {1}", new Object[]{"ISS", calculator.calculate(budget, new TaxISS())});
+		logger.log(Level.INFO, "{0} = {1}", new Object[]{"ICMS", calculator.calculate(budget, new TaxICMS())});
+		
 	}
 
 }
